a6b9845200d9f94d42f9a8a086dc125eb562e96f
Rustem Akhmetshin <143821@mail.ru>
2022-01-19 20:03:44 +0300

    fixed according to validator

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 
 Добавлена интерактивность при помощи JavaScript
 
+Ссылка на проект: https://ruskutuz.github.io/mesto/
+
 * [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
 
 Рустем Ахметшин, 2022г.
--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 const profileInfoEditButton = document.querySelector('.profile__edit-button');
 
 const popup = document.querySelector('.popup');
+const popupForm = document.querySelector('.popup__container');
 const popupCloseButton = popup.querySelector('.popup__close-icon');
 const popupOverlay = popup.querySelector('.popup__overlay');
-const popupInputFieldName = popup.querySelector('.popup__field-one');
-const popupInputFieldProfession = popup.querySelector('.popup__field-two');
-const popupSubmitButton = popup.querySelector('.popup__submit-button');
+const popupInputFieldName = popup.querySelector('.popup__input-name');
+const popupInputFieldProfession = popup.querySelector('.popup__input-profession');
 
 const profileHeader = document.querySelector('.profile__header');
 const profileProfession = document.querySelector('.profile__subscription');
@@ -29,7 +29,7 @@ popupOverlay.addEventListener('click', (event) => {
     }
 });
 
-popupSubmitButton.addEventListener('submit', (evt) => {
+popupForm.addEventListener('submit', (evt) => {
     evt.preventDefault();
     profileHeader.textContent = popupInputFieldName.value;
     profileProfession.textContent = popupInputFieldProfession.value;
