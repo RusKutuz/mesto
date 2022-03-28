d959eeddca7273c4b1ca878c4b05435a69238830
Rustem Akhmetshin <143821@mail.ru>
2022-03-28 16:58:49 +0300

    sprint 8 webpack

--- src/index.js
+++ src/index.js
@@ -20,49 +20,7 @@ import {
   popupInputLink
 } from '../scripts/data.js';
 
-import Close_Icon from '../images/Close_Icon.svg';
-import Custoue from '../images/Custoue.jpg';
-import Dombai from '../images/Dombai.jpg';
-import Gora_Elbrus from '../images/Gora_Elbrus.jpg';
-import Karachaevsk from '../images/Karachaevsk.jpg';
-import Like_active from '../images/Like_active.svg';
-import Like from '../images/Like.svg';
-import Logo_dark from '../images/Logo_dark.svg';
-import Logo from '../images/Logo.svg';
-import Pencil from '../images/Pencil.svg';
-import Plus from '../images/Plus.svg';
-import Trash from '../images/Trash.svg';
-
-import InterBlack from '../vendor/fonts/Inter-Black.woff';
-import InterBlack2 from '../vendor/fonts/Inter-Black.woff2';
-import InterMedium from '../vendor/fonts/Inter-Medium.woff';
-import InterMedium2 from '../vendor/fonts/Inter-Medium.woff2';
-import InterRegular from '../vendor/fonts/Inter-Regular.woff';
-import InterRegular2 from '../vendor/fonts/Inter-Regular.woff2';
-
 import '../pages/index.css'; // добавьте импорт главного файла стилей 
-
-const whoIsTheGoat = [
-  // меняем исходные пути на переменные
-  { name: 'Close_Icon', image: close_Icon },
-  { name: 'Custoue', image: custoue },
-  { name: 'Dombai', image: dombai },
-  { name: 'Gora_Elbrus', image: gora_Elbrus },
-  { name: 'Karachaevsk', image: karachaevsk },
-  { name: 'Like_active', image: like_active },
-  { name: 'Like', image: like },
-  { name: 'Logo_dark', image: logo_dark },
-  { name: 'Logo', image: logo },
-  { name: 'Pencil', image: pencil },
-  { name: 'Plus', image: plus },
-  { name: 'Trash', image: trash },
-  { name: 'InterBlack', font: interBlack },
-  { name: 'InterBlack2', font: interBlack2 },
-  { name: 'InterMedium', font: interMedium },
-  { name: 'InterMedium2', font: interMedium2 },
-  { name: 'InterRegular', font: interRegular },
-  { name: 'InterRegular2', font: interRegular2 },
-]; 
 
 const popupWithImage = new PopupWithImage('.popup_type_picture');
 const popupEditProfile = new PopupWithForm('.popup_type_edit-profile', submitFormEditProfile);
@@ -144,4 +102,54 @@ function openPopupAddCard() {
 
 addCardButton.addEventListener('click', openPopupAddCard);
 profileInfoEditButton.addEventListener('click', openPopupEditProfile);
- 
+
+
+
+
+
+
+/*
+import Close_Icon from '../images/Close_Icon.svg';
+import Custoue from '../images/Custoue.jpg';
+import Dombai from '../images/Dombai.jpg';
+import Gora_Elbrus from '../images/Gora_Elbrus.jpg';
+import Karachaevsk from '../images/Karachaevsk.jpg';
+import Like_active from '../images/Like_active.svg';
+import Like from '../images/Like.svg';
+import Logo_dark from '../images/Logo_dark.svg';
+import Logo from '../images/Logo.svg';
+import Pencil from '../images/Pencil.svg';
+import Plus from '../images/Plus.svg';
+import Trash from '../images/Trash.svg';
+
+import InterBlack from '../vendor/fonts/Inter-Black.woff';
+import InterBlack2 from '../vendor/fonts/Inter-Black.woff2';
+import InterMedium from '../vendor/fonts/Inter-Medium.woff';
+import InterMedium2 from '../vendor/fonts/Inter-Medium.woff2';
+import InterRegular from '../vendor/fonts/Inter-Regular.woff';
+import InterRegular2 from '../vendor/fonts/Inter-Regular.woff2';
+
+
+
+const whoIsTheGoat = [
+  // меняем исходные пути на переменные
+  { name: 'Close_Icon', image: close_Icon },
+  { name: 'Custoue', image: custoue },
+  { name: 'Dombai', image: dombai },
+  { name: 'Gora_Elbrus', image: gora_Elbrus },
+  { name: 'Karachaevsk', image: karachaevsk },
+  { name: 'Like_active', image: like_active },
+  { name: 'Like', image: like },
+  { name: 'Logo_dark', image: logo_dark },
+  { name: 'Logo', image: logo },
+  { name: 'Pencil', image: pencil },
+  { name: 'Plus', image: plus },
+  { name: 'Trash', image: trash },
+  { name: 'InterBlack', font: interBlack },
+  { name: 'InterBlack2', font: interBlack2 },
+  { name: 'InterMedium', font: interMedium },
+  { name: 'InterMedium2', font: interMedium2 },
+  { name: 'InterRegular', font: interRegular },
+  { name: 'InterRegular2', font: interRegular2 },
+]; 
+*/
